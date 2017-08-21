db8d80875c8f9a286ddcbf56a5e215b56917d477
davedumas0 <davedumas0@yahoo.com>
2017-08-21 01:49:15 -0700

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -5,14 +5,141 @@ local options = {
     height = 0.04,
     scale = 0.4,
     font = 0,
-    menu_title = "Model Menu",
+    menu_title = "Main obj Menu",
     menu_subtitle = "Categories",
     color_r = 30,
     color_g = 144,
     color_b = 255,
 }
+ Citizen.CreateThread(function()
+	while true do
+	  Citizen.Wait(0) 
+	  if DoesEntityExist(a) then 	  
+  local position = 'objID '..a
+  local offset = {x = 0.290, y = 0.850}
+  local rgb = {r = 25, g = 25, b = 140}
+  local alpha = 255
+  local scale = 0.2
+  local font = 0
+            SetTextColour(rgb.r, rgb.g, rgb.b, alpha)
+            SetTextFont(font)
+		    SetTextScale(scale, scale)
+			SetTextWrap(0.0, 1.0)
+			SetTextCentre(false)
+			SetTextDropshadow(2, 2, 0, 0, 0)
+			SetTextEdge(1, 0, 0, 0, 205)
+			SetTextEntry("STRING")
+			AddTextComponentString(position)			
+			DrawText(offset.x, offset.y)       
+	  end	  
+	end		
+end)
 
+ Citizen.CreateThread(function()
+	while true do
+	  Citizen.Wait(0) 
+	  if DoesEntityExist(a) then 	  
+  local position = "obj rotation "..entityRot
+  local offset = {x = 0.290, y = 0.860}
+  local rgb = {r = 25, g = 25, b = 150}
+  local alpha = 255
+  local scale = 0.2
+  local font = 0
+            SetTextColour(rgb.r, rgb.g, rgb.b, alpha)
+            SetTextFont(font)
+		    SetTextScale(scale, scale)
+			SetTextWrap(0.0, 1.0)
+			SetTextCentre(false)
+			SetTextDropshadow(2, 2, 0, 0, 0)
+			SetTextEdge(1, 0, 0, 0, 205)
+			SetTextEntry("STRING")
+			AddTextComponentString(position)			
+			DrawText(offset.x, offset.y)       
+	  end	  
+	end		
+end)
+ Citizen.CreateThread(function()
+	while true do
+	  Citizen.Wait(0) 
+	  if DoesEntityExist(a) then 	  
+  local position = "obj coords "..entityCoords
+  local offset = {x = 0.290, y = 0.870}
+  local rgb = {r = 25, g = 25, b = 120}
+  local alpha = 255
+  local scale = 0.2
+  local font = 0
+            SetTextColour(rgb.r, rgb.g, rgb.b, alpha)
+            SetTextFont(font)
+		    SetTextScale(scale, scale)
+			SetTextWrap(0.0, 1.0)
+			SetTextCentre(false)
+			SetTextDropshadow(2, 2, 0, 0, 0)
+			SetTextEdge(1, 0, 0, 0, 205)
+			SetTextEntry("STRING")
+			AddTextComponentString(position)			
+			DrawText(offset.x, offset.y)       
+	  end	  
+	end		
+end)
 
+ Citizen.CreateThread(function()
+	while true do
+	  Citizen.Wait(0) 
+	  if DoesEntityExist(a) then
+	   if DoesEntityHavePhysics(a) then
+	     c = "yes"
+	   else
+	     c = "no"
+	   end
+  local position = "does obj have physics "..c
+  local offset = {x = 0.290, y = 0.880}
+  local rgb = {r = 25, g = 25, b = 130}
+  local alpha = 255
+  local scale = 0.2
+  local font = 0
+            SetTextColour(rgb.r, rgb.g, rgb.b, alpha)
+            SetTextFont(font)
+		    SetTextScale(scale, scale)
+			SetTextWrap(0.0, 1.0)
+			SetTextCentre(false)
+			SetTextDropshadow(2, 2, 0, 0, 0)
+			SetTextEdge(1, 0, 0, 0, 205)
+			SetTextEntry("STRING")
+			AddTextComponentString(position)			
+			DrawText(offset.x, offset.y)       
+	  end	  
+	end		
+end)
+ Citizen.CreateThread(function()
+	while true do
+	isEntityAttached = IsEntityAttached(a)
+	  Citizen.Wait(0) 
+	  if DoesEntityExist(a) then
+	   if IsEntityAttached(a) then
+	     d = "yes"
+	   else
+	     d = "no"
+	   end
+  local position = "is Entity Attached "..d
+  local offset = {x = 0.290, y = 0.890}
+  local rgb = {r = 25, g = 25, b = 160}
+  local alpha = 255
+  local scale = 0.2
+  local font = 0
+            SetTextColour(rgb.r, rgb.g, rgb.b, alpha)
+            SetTextFont(font)
+		    SetTextScale(scale, scale)
+			SetTextWrap(0.0, 1.0)
+			SetTextCentre(false)
+			SetTextDropshadow(2, 2, 0, 0, 0)
+			SetTextEdge(1, 0, 0, 0, 205)
+			SetTextEntry("STRING")
+			AddTextComponentString(position)			
+			DrawText(offset.x, offset.y)       
+	  end	  
+	end		
+end)
+isEntityAttached = IsEntityAttached(attach)
 
 function Notify(text)
     SetNotificationTextEntry('STRING')
@@ -41,67 +168,133 @@ function DisplayHelpText(str)
 	AddTextComponentString(str)
 	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 end
-
-
-
+------------------------------------------------------------------
+--------------------------main menu-------------------------------
+------------------------------------------------------------------
 function Main()
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
 	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
     options.menu_title = "my menyoo"
     options.menu_subtitle = "~o~main menu"
     ClearMenu()
-    Menu.addButton("~y~objects menu", "objMenu", nil)
-	Menu.addButton("~y~animations menu", "animationMenu", nil)
+    Menu.addButton("~y~GunRunning ~b~menu", "GRCrateMenu", nil)
+	Menu.addButton("~y~GunRunning MOC ~b~menu", "MOCmoules", nil)
 end
-
-function Main2()
+------------------------------------------------------------------
+--------------------------gunrunning obj menu---------------------
+------------------------------------------------------------------
+function GRCrateMenu()
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
 	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
-    options.menu_title = "my menyoo"
-    options.menu_subtitle = "~o~main menu"
+    options.menu_title = "~y~GunRunning"
+    options.menu_subtitle = "select object to spawn"
     ClearMenu()
-	Menu.addButton("~y~animations menu", "animationMenu", nil)
-end
-
-function animationMenu()
+	Menu.addButton("adv_case", "objMenuGRAdvCase", nil)
+	Menu.addButton("carcreeper", "objMenuGRCarCreeper", nil)
+	Menu.addButton("gun crate 01a", "objMenuGRCrateGun01a", nil)
+	Menu.addButton("mag crate 01a", "objMenuGRCrateMag01a", nil)		
+	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
+end	
+------------------------------------------------------------------
+--------------------------gunrunning MOC obj menu-----------------
+------------------------------------------------------------------
+function MOCmoules()
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
 	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
-    options.menu_title = "~y~animation menu"
-    options.menu_subtitle = "VirtMill animation list"
+    options.menu_title = "~y~GunRunning MOC"
+    options.menu_subtitle = "select object to spawn"
     ClearMenu()
-    Menu.addButton("unload_rl_01_amy_skater_01", "thingsVM3", nil)
-    Menu.addButton("unload_rl_02_amy_skater_01","thingsVM4",nil)
-    Menu.addButton("operate_01_amy_skater_01", "thingsVM5", nil)
-    Menu.addButton("operate_02_amy_skater_01", "thingsVM6", nil)
-    Menu.addButton("operate_03_amy_skater_01", "thingsVM7", nil)
-    Menu.addButton("reachout_amy_skater_01", "thingsVM8", nil)	
-    Menu.addButton("~o~NEXT PAGE", "nextPageAnim", nil)	
-	Menu.addButton("~r~BACK TO MAIN MENU", "Main", nil)	
-end
-function objMenu()
+    Menu.addButton("MOC carmod", "MOCCarMod01", nil)
+	Menu.addButton("MOC command", "MOCCommand01", nil)
+	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
+end	
+------------------------------------------------------------------
+-----------------------advanced case spawn menu-------------------
+------------------------------------------------------------------
+function objMenuGRAdvCase()
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
 	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
-    options.menu_title = "~y~obj spawner menu"
-    options.menu_subtitle = "obj list"
+    options.menu_title = "~y~GunRunning"
+    options.menu_subtitle = "advanced case"
     ClearMenu()
-	Menu.addButton("637672069", "things1", 637672069)
-    Menu.addButton("gr_crate_gun_01a", "things1", "gr_prop_gr_crate_gun_01a")
-    Menu.addButton("~r~delete stuff", "deleteStuff", nil)
+	Menu.addButton("spawn advanced case", "things1", -739654066)
+	Menu.addButton("freeze advanced case", "freezeStuff", a)
+	Menu.addButton("unfreeze advanced case", "unfreezeStuff", a)
+    Menu.addButton("~r~delete object", "deleteStuff", nil)
+	Menu.addButton("~g~back", "GRCrateMenu", nil)
 	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
 end
-function nextPageAnim()
+------------------------------------------------------------------
+--------------------------car creeper spawn menu------------------
+------------------------------------------------------------------
+function objMenuGRCarCreeper()
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
 	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
-    options.menu_title = "~y~animation menu"
-    options.menu_subtitle = "speed drill animation list"
-    ClearMenu()	
-	Menu.addButton("unload_rl_01_amy_skater_01", "thingsSD1", nil)
-	Menu.addButton("unload_rl_02_amy_skater_01", "thingsSD2", nil)
-	Menu.addButton("operate_01_amy_skater_01", "thingsSD3", nil)	
-    Menu.addButton("~o~PREV PAGE", "animationMenu", nil)	
-	Menu.addButton("~r~BACK TO MAIN MENU", "Main", nil)		
-
+    options.menu_title = "~y~GunRunning"
+    options.menu_subtitle = "car creeper"
+    ClearMenu()
+	Menu.addButton("spawn car creeper", "things1", 1177042352)
+    Menu.addButton("~r~delete object", "deleteStuff", nil)
+	Menu.addButton("~g~back", "GRCrateMenu", nil)
+	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
 end
+------------------------------------------------------------------
+---------------------gun crate 01a spawn menu---------------------
+------------------------------------------------------------------
+function objMenuGRCrateGun01a()
+    DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
+	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
+    options.menu_title = "~y~GunRunning"
+    options.menu_subtitle = "gun crate"
+    ClearMenu()
+	Menu.addButton("spawn gun crate 01a", "things1", -471632408)
+    Menu.addButton("~r~delete object", "deleteStuff", nil)
+	Menu.addButton("~g~back", "GRCrateMenu", nil)
+	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
+end
+------------------------------------------------------------------
+------------------mag crate 01a spawn menu------------------------
+------------------------------------------------------------------
+function objMenuGRCrateMag01a()
+    DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
+	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
+    options.menu_title = "~y~GunRunning"
+    options.menu_subtitle = "mag crate"
+    ClearMenu()
+	Menu.addButton("spawn mag crate 01", "things1", 1281731670)
+    Menu.addButton("~r~delete object", "deleteStuff", nil)
+	Menu.addButton("~g~back", "GRCrateMenu", nil)
+	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
+end
+------------------------------------------------------------------
+------------------MOC carmod spawn menu------------------------
+------------------------------------------------------------------
+function MOCCarMod01()
+    DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
+	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
+    options.menu_title = "~y~GunRunning MOC"
+    options.menu_subtitle = "MOC carmod"
+    ClearMenu()
+	Menu.addButton("spawn MOC carmod 01", "things1", -2083549178)
+	Menu.addButton("~r~delete object", "deleteStuff", nil)
+	Menu.addButton("~g~BACK", "MOCmoules", nil)	
+	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
+end
+------------------------------------------------------------------
+------------------MOC command spawn menu------------------------
+------------------------------------------------------------------
+function MOCCommand01()
+    DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
+	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
+    options.menu_title = "~y~GunRunning MOC"
+    options.menu_subtitle = "MOC command"
+    ClearMenu()
+	Menu.addButton("spawn MOC command 01", "things1", -2104782239)
+	Menu.addButton("~r~delete object", "deleteStuff", nil)
+	Menu.addButton("~g~BACK", "MOCmoules", nil)	
+	Menu.addButton("~g~BACK TO MAIN MENU", "Main", nil)	
+end
+
 --[[
 function Main()
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
@@ -117,56 +310,17 @@ function Main()
     Menu.addButton("reachout_amy_skater_01", "things6", nil)
 end
 --]]
+
+------------------------------------------------------------------
+--------------------------obj spawner-----------------------------
+------------------------------------------------------------------
 function things1(stuff)
  things(stuff)
 spawnStuff(stuff)
 end
-function thingsSD3()
-RequestAnimDict("anim@amb@machinery@speed_drill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@speed_drill@", "operate_01_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
 
-end
-function thingsSD2()
-RequestAnimDict("anim@amb@machinery@speed_drill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@speed_drill@", "unload_rl_02_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
 
-end
-function thingsSD1()
-RequestAnimDict("anim@amb@machinery@speed_drill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@speed_drill@", "unload_rl_01_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
 
-end
-
-function thingsVM3()
-RequestAnimDict("anim@amb@machinery@vertical_mill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@vertical_mill@", "unload_rl_01_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
-
-end
-function thingsVM4()
-RequestAnimDict("anim@amb@machinery@vertical_mill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@vertical_mill@", "unload_rl_02_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
-
-end
-function thingsVM5()
-RequestAnimDict("anim@amb@machinery@vertical_mill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@vertical_mill@", "operate_01_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
-
-end
-function thingsVM6()
-RequestAnimDict("anim@amb@machinery@vertical_mill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@vertical_mill@", "operate_02_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
-
-end
-function thingsVM7()
-RequestAnimDict("anim@amb@machinery@vertical_mill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@vertical_mill@", "operate_03_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
-
-end
-function thingsVM8()
-RequestAnimDict("anim@amb@machinery@vertical_mill@")
-TaskPlayAnim(GetPlayerPed(PlayerId()), "anim@amb@machinery@vertical_mill@", "reachout_amy_skater_01", 8.0, -8.0, -1, 48, 0.0, 0, 0, 0)
-
-end
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --Press F5 to open/close menu
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
